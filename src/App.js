9f45a6e6cd247cc29dfcedd7d4ef36194df3b88a
Kaushal Jha <you@example.com>
2024-05-19 23:11:24 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -23,25 +23,44 @@ function App() {
       </nav>
       <main>
         <section className="hero-section">
-          <div className="badge">
-            🎓 An <span className="bold">IIT Delhi</span> Alumni Initiative
-          </div>
-          <h1>
-            Become an Expert in the field of <span>Data Science with 6
-            courses</span>
-          </h1>
-          <p>
-            A specially crafted Tech Kickstarter, with 5+ extensive online
-            courses.
-          </p>
-          <div className="features">
-            <div className="feature-item">Personal Mentorship</div>
-            <div className="feature-item">Internship Assistance</div>
-            <div className="feature-item">Industry Certified Courses</div>
-          </div>
-          <div className="cta-buttons">
-            <button className="enroll-button">Enroll Now</button>
-            <button className="know-more-button">Know More</button>
+          <div className="hero-content">
+            <div className="badge">
+              🎓 An <span className="bold">IIT Delhi</span> Alumni Initiative
+            </div>
+            <h1>
+              <span className="grey-text">Become an Expert in the</span>
+              
+              field of <span className="highlight-text"> Data Science </span>
+              <br />
+              <span className="highlight-text">With 6 courses! </span>
+            </h1>
+            <p>
+              A specially crafted Tech Kickstarter, with 5+ extensive online courses.
+            </p>
+            <div className="features">
+              <div className="feature-item">
+                <img className="image" src="image.png" alt="Personal Mentorship" />
+                Personal Mentorship
+              </div>
+              <div className="feature-item">
+                <img className="image" src="image.png" alt="Internship Assistance" />
+                Internship Assistance
+              </div>
+              <div className="feature-item">
+                <img className="image" src="image.png" alt="Industry Certified Courses" />
+                Industry Certified Courses
+              </div>
+            </div>
+            <div className="cta-buttons">
+              <button className="enroll-button">
+                <img src="path_to_enroll_now_icon" alt="Enroll Now" />
+                Enroll Now
+              </button>
+              <button className="know-more-button">
+                <img src="path_to_know_more_icon" alt="Know More" />
+                Know More
+              </button>
+            </div>
           </div>
         </section>
         <section className="stats-section">
